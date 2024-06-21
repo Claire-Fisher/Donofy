c52dc63c145cbaf839b7e3853bae1d0b02d93231
Claire Fisher <cafisher83@yahoo.co.uk>
2024-06-21 19:19:17 +0000

    add: toasts templates, settings and styles

--- donofy/home/views.py
+++ donofy/home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib import messages
 
 
 def index(request):
     """ A view to return the index page """
+    messages.info(request, 'Yay! A message!')
 
     return render(request, 'home/index.html')
