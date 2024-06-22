2b45cd58ceb0483fb6adec2b7ba1e892f14fb7cd
Claire Fisher <cafisher83@yahoo.co.uk>
2024-06-22 17:52:05 +0000

    add: customises admin

--- donofy/charities/admin.py
+++ donofy/charities/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
 from .models import Charity, Category
 
-admin.site.register(Charity)
-admin.site.register(Category)
+
+class CharityAdmin(admin.ModelAdmin):
+    list_display = (
+        'charity_name',
+        'charity_num',
+        'category',
+        'image_url',
+        'total_received',
+    )
+
+    ordering = ('charity_name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Charity, CharityAdmin)
+admin.site.register(Category, CategoryAdmin)
