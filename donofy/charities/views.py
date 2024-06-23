0543e150b795ed48bd5547d2a0f4aa65ece01701
Claire Fisher <cafisher83@yahoo.co.uk>
2024-06-23 14:16:15 +0000

    add: edits all_charities view to separate active and inactive charities

--- donofy/charities/views.py
+++ donofy/charities/views.py
@@ -5,10 +5,12 @@ from .models import Charity
 def all_charities(request):
     """ A view to show all charities """
 
-    charities = Charity.objects.all()
+    charities = Charity.objects.filter(active=True)
+    inactive_charities = Charity.objects.filter(active=False)
 
     context = {
         'charities': charities,
+        'inactive_charities': inactive_charities,
     }
 
     return render(request, 'charities/charities.html', context)
