3715d514bb56f0349d284e81f2f286ea4beb1476
Claire Fisher <cafisher83@yahoo.co.uk>
2024-06-23 14:13:58 +0000

    add: edit to TestCharitiesView to filter charities by active status. Only 'active' charities will be displayed to users

--- donofy/charities/test_charities.py
+++ donofy/charities/test_charities.py
@@ -13,14 +13,13 @@ class TestCharitiesURL(TestCase):
 class TestCharitiesView(TestCase):
 
     def setUp(self):
-        self.charity1 = Charity.objects.create(charity_name='Test-Charity 1', charity_num=123, description='Test description', total_received_monthly={})
-        self.charity2 = Charity.objects.create(charity_name='Test-Charity 2', charity_num=123, description='Test description', total_received_monthly={})
-        self.charity3 = Charity.objects.create(charity_name='Test-Charity 3', charity_num=123, description='Test description', total_received_monthly={})
+        self.charity1 = Charity.objects.create(charity_name='Test-Charity 1', active=True, charity_num=123, description='Test description', total_received_monthly={})
+        self.charity2 = Charity.objects.create(charity_name='Test-Charity 2', active=True, charity_num=123, description='Test description', total_received_monthly={})
+        self.charity3 = Charity.objects.create(charity_name='Test-Charity 3', active=False, charity_num=123, description='Test description', total_received_monthly={})
 
     def test_view_gets_all_charities(self):
         response = self.client.get(reverse('charities'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, self.charity1.charity_name)
         self.assertContains(response, self.charity2.charity_name)
-        self.assertContains(response, self.charity3.charity_name)
-        self.assertEqual(len(response.context['charities']), 3)
+        self.assertEqual(len(response.context['charities']), 2)
