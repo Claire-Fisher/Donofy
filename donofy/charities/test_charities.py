f7433afacd53111446d9ea7a27a653d132615133
Claire Fisher <cafisher83@yahoo.co.uk>
2024-06-24 12:15:45 +0000

    add: test for charities searchbar

--- donofy/charities/test_charities.py
+++ donofy/charities/test_charities.py
@@ -13,9 +13,9 @@ class TestCharitiesURL(TestCase):
 class TestCharitiesView(TestCase):
 
     def setUp(self):
-        self.charity1 = Charity.objects.create(charity_name='Test-Charity 1', active=True, charity_num=123, description='Test description', total_received_monthly={})
-        self.charity2 = Charity.objects.create(charity_name='Test-Charity 2', active=True, charity_num=123, description='Test description', total_received_monthly={})
-        self.charity3 = Charity.objects.create(charity_name='Test-Charity 3', active=False, charity_num=123, description='Test description', total_received_monthly={})
+        self.charity1 = Charity.objects.create(charity_name='Test-Charity 1', active=True, charity_num=123, description='Test description 1', total_received_monthly={})
+        self.charity2 = Charity.objects.create(charity_name='Test-Charity 2', active=True, charity_num=123, description='Test description 2', total_received_monthly={})
+        self.charity3 = Charity.objects.create(charity_name='Test-Charity 3', active=False, charity_num=123, description='Test description 3', total_received_monthly={})
 
     def test_view_gets_all_charities(self):
         response = self.client.get(reverse('charities'))
@@ -23,3 +23,25 @@ class TestCharitiesView(TestCase):
         self.assertContains(response, self.charity1.charity_name)
         self.assertContains(response, self.charity2.charity_name)
         self.assertEqual(len(response.context['charities']), 2)
+    
+    def test_search_query(self):
+        # Test search by name
+        response = self.client.get(reverse('charities'), {'q': 'Test-Charity 1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.charity1.charity_name)
+        self.assertNotContains(response, self.charity2.charity_name)
+        self.assertEqual(len(response.context['charities']), 1)
+
+        # Test search by description
+        response = self.client.get(reverse('charities'), {'q': 'description 1'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.charity1.charity_name)
+        self.assertNotContains(response, self.charity2.charity_name)
+        self.assertEqual(len(response.context['charities']), 1)
+
+        # Test search with no results
+        response = self.client.get(reverse('charities'), {'q': 'Nonexistent Charity'})
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.charity1.charity_name)
+        self.assertNotContains(response, self.charity2.charity_name)
+        self.assertEqual(len(response.context['charities']), 0)
