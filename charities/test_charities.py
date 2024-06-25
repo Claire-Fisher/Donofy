eed53af75668869969467ad36b774897b199209a
Claire Fisher <cafisher83@yahoo.co.uk>
2024-06-25 12:20:52 +0000

    fix: corrects test until no Errors

--- charities/test_charities.py
+++ charities/test_charities.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import Charity
+from .models import Charity, Category
 
 
 class TestCharitiesURL(TestCase):
@@ -13,9 +13,33 @@ class TestCharitiesURL(TestCase):
 class TestCharitiesView(TestCase):
 
     def setUp(self):
-        self.charity1 = Charity.objects.create(charity_name='Test-Charity 1', active=True, charity_num=123, category=1, description='Test description 1', total_received_monthly={})
-        self.charity2 = Charity.objects.create(charity_name='Test-Charity 2', active=True, charity_num=123, category=2, description='Test description 2', total_received_monthly={})
-        self.charity3 = Charity.objects.create(charity_name='Test-Charity 3', active=False, charity_num=123, category=3, description='Test description 3', total_received_monthly={})
+        self.category1 = Category.objects.create(name='Category1')
+        self.category2 = Category.objects.create(name='Category2')
+        self.category3 = Category.objects.create(name='Category3')
+
+        self.charity1 = Charity.objects.create(
+            charity_name='Test-Charity 1',
+            active=True, charity_num=123,
+            category=self.category1,
+            description='Test description 1',
+            total_received_monthly={},
+        )
+
+        self.charity2 = Charity.objects.create(
+            charity_name='Test-Charity 2',
+            active=True, charity_num=123,
+            category=self.category2,
+            description='Test description 2',
+            total_received_monthly={},
+        )
+
+        self.charity3 = Charity.objects.create(
+            charity_name='Test-Charity 3',
+            active=False, charity_num=123,
+            category=self.category3,
+            description='Test description 3',
+            total_received_monthly={},
+        )
 
     def test_view_gets_all_charities(self):
         response = self.client.get(reverse('charities'))
@@ -45,3 +69,11 @@ class TestCharitiesView(TestCase):
         self.assertNotContains(response, self.charity1.charity_name)
         self.assertNotContains(response, self.charity2.charity_name)
         self.assertEqual(len(response.context['charities']), 0)
+
+    def test_sort_by_category(self):
+        response = self.client.get(reverse('charities'), {'category': self.category1.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.charity1.charity_name)
+        self.assertNotContains(response, self.charity2.charity_name)
+        self.assertNotContains(response, self.charity3.charity_name)
+        self.assertEqual(len(response.context['charities'][0]), self.charity1)
