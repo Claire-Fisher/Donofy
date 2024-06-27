cc49825a925984376b80d743515b300b3873628e
Claire Fisher <cafisher83@yahoo.co.uk>
2024-06-27 20:14:40 +0000

    add: starts user profile admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+
+    readonly_fields = (
+        'date_joined',
+    )
+
+    fields = (
+        'user',
+        'active_user',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'street_address_1',
+        'street_address_2',
+        'town_or_city',
+        'country',
+    )
+
+    list_display = (
+        'charity_favs',
+        'donation_number',
+        'date',
+        'fullname',
+        'total',
+    )
+
+    ordering = ('-date',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
