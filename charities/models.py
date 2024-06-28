7c3d3f1f750ca5b51d5eaa5753e0470d1bbbb414
Claire Fisher <cafisher83@yahoo.co.uk>
2024-06-28 13:50:49 +0000

    add: defaul arg to Charity.total_received

--- charities/models.py
+++ charities/models.py
@@ -34,7 +34,7 @@ class Charity(models.Model):
     impact2 = models.TextField(null=True, blank=True)
     impact3 = models.TextField(null=True, blank=True)
     total_received_monthly = models.JSONField(blank=True)
-    total_received = models.PositiveIntegerField(null=True, blank=True)
+    total_received = models.PositiveIntegerField(default=0, null=True, blank=True)
 
     def __str__(self):
         return self.charity_name
