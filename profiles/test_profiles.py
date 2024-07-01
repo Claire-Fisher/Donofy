aafeaf14f2ef8a6962c09843fd6902b71e7e9a68
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-01 14:08:04 +0000

    add: test for profiles url

--- profiles/test_profiles.py
+++ profiles/test_profiles.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 from django.urls import reverse
 
+
+class TestProfilesURL(TestCase):
+
+    def test_profile_template_used(self):
+        response = self.client.get(reverse('profile'))
+        self.assertTemplateUsed(response, 'profiles/profile.html')
