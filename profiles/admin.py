64d6d211d92fd4cabef5529819397091ff62afb4
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-01 15:26:22 +0000

    chore: adjusts UserProfile admin layout

--- profiles/admin.py
+++ profiles/admin.py
@@ -12,17 +12,23 @@ class UserProfileAdmin(admin.ModelAdmin):
     fields = (
         'user',
         'active_user',
+        'is_authenticated',
         'first_name',
         'last_name',
-        'phone_number',
+        'phone_num',
         'street_address_1',
         'street_address_2',
         'town_or_city',
         'country',
+        'charity_favs'
     )
 
     list_display = (
-        'charity_favs',
+        'user',
+        'last_name',
+        'first_name',
+        'active_user',
+        'is_authenticated'
     )
 
     ordering = ('last_name',)
