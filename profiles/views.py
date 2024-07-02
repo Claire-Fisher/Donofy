3ca3c3760aa33b8604256087526a86e5256a6051
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-02 16:43:58 +0000

    add: view to save changes to user profile data

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from .models import UserProfile
+from .forms import UserForm, UserProfileForm
 
 @login_required
 def profile(request):
-    """ Display the user's profile """
+    user = request.user
+    user_profile = get_object_or_404(UserProfile, user=user)
 
-    return render(request, 'profiles/profile.html')
+    if request.method == "POST":
+        user_form = UserForm(request.POST, instance=user)
+        user_profile_form = UserProfileForm(
+            request.POST, instance=user_profile)
+        if user_form.is_valid() and user_profile_form.is_valid():
+            user_form.save()
+            user_profile_form.save()
+            messages.success(request, 'Billing details updated successfully.')
+        else:
+            messages.error(
+                request, 'Update failed. Please ensure the form is valid.')
+    else:
+        user_form = UserForm(instance=user)
+        user_profile_form = UserProfileForm(instance=user_profile)
+
+    context = {
+        'user_form': user_form,
+        'user_profile_form': user_profile_form,
+    }
+
+    return render(request, 'profiles/profile.html', context)
