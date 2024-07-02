689f29d728e0debca7a92a37e56d9348fef2cacd
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-02 14:39:53 +0000

    chore: simplified profiles form to get page rendering first

--- profiles/forms.py
+++ profiles/forms.py
@@ -1,45 +1,24 @@
 from django import forms
+from django.contrib.auth.models import User
 from .models import UserProfile
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name')
 
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
+        exclude = ('user',)
         fields = (
-            'first_name',
-            'last_name',
             'phone_num',
             'street_address_1',
             'street_address_2',
             'town_or_city',
             'county',
             'post_code_zip',
-        )
-
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first_name field
-        """
-        super().__init__(*args, **kwargs)
-        placeholders = {
-            'first_name': 'First Name',
-            'last_name': 'Last Name',
-            'phone_numb': 'Phone Number',
-            'street_address1': 'Street Address 1',
-            'street_address2': 'Street Address 2',
-            'town_or_city': 'Town or City',
-            'county': 'County, State or Locality',
-            'post_code_zip': 'Postal Code',
-        }
-
-        self.fields['first_name'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
+            'country',
+            )
