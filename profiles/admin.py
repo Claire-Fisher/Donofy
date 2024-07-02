ad6e590bf4594f7a5940b4232efb4e003ad1a287
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-02 12:53:42 +0000

    chore: adjusts some user profile fields. Merges the user profile sections in the admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,37 +1,17 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from .models import UserProfile
+from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 
 
-class UserProfileAdmin(admin.ModelAdmin):
-
-    readonly_fields = (
-        'date_joined',
-    )
-
-    fields = (
-        'user',
-        'active_user',
-        'is_authenticated',
-        'first_name',
-        'last_name',
-        'phone_num',
-        'street_address_1',
-        'street_address_2',
-        'town_or_city',
-        'country',
-        'charity_favs'
-    )
-
-    list_display = (
-        'user',
-        'last_name',
-        'first_name',
-        'active_user',
-        'is_authenticated'
-    )
-
-    ordering = ('last_name',)
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
 
 
-admin.site.register(UserProfile, UserProfileAdmin)
+class UserProfileAdmin(AuthUserAdmin):
+    inlines = [UserProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserProfileAdmin)
