1831a5f42d313e9e0f8d7d876d4e0d8d09125170
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-03 16:56:10 +0000

    add: subscription admin to backend

--- subscriptions/admin.py
+++ subscriptions/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Subscription
 
-# Register your models here.
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'sub_active', 'sub_total')
+    search_fields = ('user__username', 'user__last_name', 'user__email')
+    list_filter = ('sub_active',)
+    readonly_fields = ('user', 'sub_total', 'sub_breakdown')
+
+    fieldsets = (
+        (None, {
+            'fields': ('user', 'sub_active', 'sub_total', 'sub_breakdown')
+        }),
+    )
+
+
+admin.site.register(Subscription, SubscriptionAdmin)
