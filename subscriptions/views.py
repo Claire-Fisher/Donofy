bc4a4b11ce8c359157cc05007f3efbff900b863b
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-03 12:16:17 +0000

    add: view to display the myDonofy tab of the profile page, to manage subscription

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from profiles.models import UserProfile
 
-# Create your views here.
+
+@login_required
+def manage_subscription(request):
+
+    active_tab = request.GET.get('tab', 'myDonofy')
+
+    context = {
+        'active_tab': active_tab
+    }
+
+    return render(request, 'profiles/profile.html', context)
