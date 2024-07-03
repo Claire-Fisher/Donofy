9067eccac21f67bdcd38106961520582f31a73d4
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-03 12:00:14 +0000

    add: start of subscription model

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from djano.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class subscription(models.Model):
+    """
+    Subscription model to hold the user's current subscription settings. 
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    sub_active = models.BooleanField(default=False)
+    sub_total = models.PositiveIntegerField(default=0, null=False)
+    sub_breakdown = models.JSONField(defaul=dict)
