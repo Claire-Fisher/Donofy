68444c2159d3cc5a0c3523a35faaad9dcbe8651a
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-04 16:00:48 +0000

    add: app_name and toggle_subscription

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from . import views
 
 
+app_name = 'subscriptions'
+
+
 urlpatterns = [
-    path('manage/', views.manage_subscription, name='manage_subscription'),
+    path(
+        'manage/',
+        views.manage_subscription,
+        name='manage_subscription'
+    ),
+    path(
+        'toggle_subscription/',
+        views.toggle_subscription_active,
+        name='toggle_subscription_active'
+    ),
 ]
