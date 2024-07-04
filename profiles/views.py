2f9aab4ec49337cbac79cae2225288cf88b36ec4
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-04 17:02:16 +0000

    chore: moves add_to_favs to subscriptions views, and splits profile view, into profile, and update_profile

--- profiles/views.py
+++ profiles/views.py
@@ -12,8 +12,38 @@ def profile(request):
     # Get user and associated UserProfile
     user = request.user
     user_profile = get_object_or_404(UserProfile, user=user)
+    user_form = UserForm(request.POST, instance=user)
+    user_profile_form = UserProfileForm(
+            request.POST, instance=user_profile)
     # Check or create a subscription object
     subscription, created = Subscription.objects.get_or_create(user=user)
+    # Get charity favs list
+    charity_favs_ids = user_profile.charity_favs or []
+    charity_favs = Charity.objects.filter(id__in=charity_favs_ids)
+
+    active_tab = request.GET.get('tab', 'myDetails')
+
+    context = {
+        'user_form': user_form,
+        'user_profile_form': user_profile_form,
+        'active_tab': active_tab,
+        'charity_favs': charity_favs,
+        'subscription': subscription,
+    }
+
+    return render(request, 'profiles/profile.html', context)
+
+
+@login_required
+def update_profile(request):
+    # Get user and associated UserProfile
+    user = request.user
+    user_profile = get_object_or_404(UserProfile, user=user)
+    # Check or create a subscription object
+    subscription, created = Subscription.objects.get_or_create(user=user)
+    # Get charity favs list
+    charity_favs_ids = user_profile.charity_favs or []
+    charity_favs = Charity.objects.filter(id__in=charity_favs_ids)
 
     if request.method == "POST":
         user_form = UserForm(request.POST, instance=user)
@@ -30,10 +60,6 @@ def profile(request):
         user_form = UserForm(instance=user)
         user_profile_form = UserProfileForm(instance=user_profile)
 
-    # Get charity favs list
-    charity_favs_ids = user_profile.charity_favs or []
-    charity_favs = Charity.objects.filter(id__in=charity_favs_ids)
-
     active_tab = request.GET.get('tab', 'myDonofy')
 
     context = {
@@ -43,32 +69,5 @@ def profile(request):
         'charity_favs': charity_favs,
         'subscription': subscription,
     }
-    print("Charity Fav IDs:", charity_favs_ids)
-    print("Charity Favs Count:", charity_favs.count())
 
-    return render(request, 'profiles/profile.html', context)
-
-
-@login_required
-def add_to_favs(request, charity_id):
-    """ Add a charity to a user's charity_favs list """
-    user = request.user
-    user_profile = get_object_or_404(UserProfile, user=user)
-    charity = get_object_or_404(Charity, pk=charity_id)
-    redirect_url = request.POST.get('redirect_url', 'profile')
-
-    if request.method == "POST":
-        charity_favs = user_profile.charity_favs or []
-
-        if charity.id in charity_favs:
-            messages.info(
-                request, 'You already have that charity in your favourites')
-        else:
-            charity_favs.append(charity.id)
-            user_profile.charity_favs = charity_favs
-            user_profile.save()
-            messages.success(
-                request, f"Added {charity.charity_name} to your favourites"
-            )
-
-    return redirect(redirect_url)
+    return redirect('profiles:profile', context)
