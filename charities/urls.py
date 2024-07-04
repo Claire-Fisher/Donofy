4dc10f70389f014a1a1b59e6ec7aa4f8689f4bb1
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-04 15:57:00 +0000

    chore: moves add_to_favs path to charities urls

--- charities/urls.py
+++ charities/urls.py
@@ -4,6 +4,19 @@ from . import views
 
 
 urlpatterns = [
-    path('charities/', views.all_charities, name='charities'),
-    path('charity_detail/<int:charity_id>/', views.charity_detail, name='charity_detail'),
+    path(
+        'charities/',
+        views.all_charities,
+        name='charities'
+    ),
+    path(
+        'charity_detail/<int:charity_id>/',
+        views.charity_detail,
+        name='charity_detail'
+    ),
+    path(
+        'add_to_favs/<int:charity_id>/',
+        views.add_to_favs,
+        name='add_to_favs'
+    ),
 ]
