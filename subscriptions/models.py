20336ae078a712f81cc2d77dd04c45293c647670
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-05 14:17:38 +0000

    add: donation model to subscription app models

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,4 +1,6 @@
+import uuid
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import User
 
 
@@ -10,3 +12,17 @@ class Subscription(models.Model):
     sub_active = models.BooleanField(default=False)
     sub_total = models.PositiveIntegerField(default=0, null=False)
     sub_breakdown = models.JSONField(default=dict, blank=True)
+
+
+class Donation(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    donation_breakdown = models.JSONField(default=dict, blank=False)
+    date = models.DateField(auto_now_add=True)
+
+    def _generate_donation_number(self):
+        """
+        Generate a random, unique donation number using UUID
+        """
+        return uuid.uuid4().hex.upper()
