06164ea3173aa6b0ce8d796829ee7d0e033649a5
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-09 19:24:54 +0000

    add: staff_action url

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('staff_action/', views.staff_action, name='staff_action'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
