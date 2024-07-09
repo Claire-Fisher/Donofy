2df6b3a74099e5b14c5251b092deadc24fb84d3a
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-09 19:25:56 +0000

    add: view to navigate to staff_action page, with security logic to give access only to superusers

--- home/views.py
+++ home/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+@login_required
+def staff_action(request):
+    """ A view to access the staff_actions page """
+    if not request.user.is_superuser:
+
+        messages.error(
+            request,
+            'You do not have permissions for that. '
+            'Please log in as an authorised user.'
+        )
+        return redirect('home/index.html')
+
+    return render(request, 'home/staff-action.html')
