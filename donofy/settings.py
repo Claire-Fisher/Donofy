ac2f2a24a80ed991c5d7b7480fa03a31cc164ae1
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-09 10:26:06 +0000

    add: stripe keys to settings

--- donofy/settings.py
+++ donofy/settings.py
@@ -185,3 +185,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Stripe
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
