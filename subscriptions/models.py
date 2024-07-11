bb1127dfca7ee0da4f389e22f0a730f5dac9db47
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-11 19:05:44 +0000

    chore: changes to models and migrates to prep for direct single donation payments

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -12,19 +12,21 @@ class Subscription(models.Model):
     Subscription model to hold the user's current subscription settings.
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    sub_active = models.BooleanField(default=False)
+    sub_active = models.BooleanField(default=True)
     sub_total = models.PositiveIntegerField(default=0, null=False)
     sub_breakdown = models.JSONField(default=dict, blank=True)
 
 
 class Donation(models.Model):
+    donation_number = models.CharField(
+        max_length=32, null=False, editable=False, default=0
+    )
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    donation_active = models.BooleanField(default=True)
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     donation_breakdown = models.JSONField(default=dict, blank=False)
     date = models.DateField(auto_now_add=True)
-    status = models.CharField(max_length=12, null=False, default='Not charged')
 
     def _generate_donation_number(self):
         """
@@ -32,31 +34,14 @@ class Donation(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
-    def _send_donation_registered_email(self):
+    def save(self, *args, **kwargs):
         """
-        Send the user notification of a pending donation email
+        If the Donation uuid hasn't been set already,
+        override save to set it first
         """
-        cust_email = self.user.email
-        subject = render_to_string(
-            'subscriptions/confirmation_emails/'
-            'donation-registered-subject.txt',
-            {'donation': self}
-        )
-        body = render_to_string(
-            'subscriptions/confirmation_emails/'
-            'donation-registered-body.txt',
-            {
-                'donation': self,
-                'contact_email': settings.DEFAULT_FROM_EMAIL
-            }
-        )
-
-        send_mail(
-            subject,
-            body,
-            settings.DEFAULT_FROM_EMAIL,
-            [cust_email]
-        )
+        if not self.donation_number:
+            self.donation_number = self._generate_donation_number()
+        super().save(*args, **kwargs)
 
     def _send_donation_changed_email(self):
         """
