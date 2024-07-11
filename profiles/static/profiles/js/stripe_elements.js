2167d928ce8591875076a4b99dd143fdb339d434
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-11 17:10:42 +0000

    chore: trying to make payment methods work

--- profiles/static/profiles/js/stripe_elements.js
+++ profiles/static/profiles/js/stripe_elements.js
@@ -22,11 +22,32 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-var form = document.getElementById('payment-form');
+var form = document.getElementById('setup-form');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
     var errorDiv = document.getElementById('card-errors');
-    stripe.createToken(card).then(function(result) {
+    stripe.confirmCardSetup(
+        clientSecret,
+        {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: '{{ user.first_name }} {{ user.last_name }}',
+                    email: '{{ user.email }}',
+                    address: {
+                        line1: '{{ user_profile.street_address_1 }}',
+                        line2: '{{ user_profile.street_address_2 }}',
+                        city: '{{ user_profile.town_or_city }}',
+                        state: '{{ user_profile.county }}',
+                        postal_code: '{{ user_profile.post_code_zip }}',
+                        country: '{{ user_profile.country.code }}'
+                    },
+                    phone: '{{ user_profile.phone_num }}',
+                    consent: '{{ user_profile.payment_consent }}'
+                }
+            }
+        }
+    ).then(function(result) {
         if (result.error) {
             var html = `
             <span class="icon icon-pink role=alert">
@@ -36,17 +57,17 @@ form.addEventListener('submit', function(event) {
             `;
             $(errorDiv).html(html);
         } else {
-            stripeTokenHandler(result.token);
+            stripeTokenHandler(result.setupIntent);
         }
     });
 });
 
-function stripeTokenHandler(token) {
-    var form = document.getElementById('payment-form');
+function stripeTokenHandler(setupIntent) {
+    var form = document.getElementById('setup-form');
     var hiddenInput = document.createElement('input');
     hiddenInput.setAttribute('type', 'hidden');
-    hiddenInput.setAttribute('name', 'stripeToken');
-    hiddenInput.setAttribute('value', token.id);
+    hiddenInput.setAttribute('name', 'setup_intent_id');
+    hiddenInput.setAttribute('value', setupIntent.id);
     form.appendChild(hiddenInput);
 
     form.submit();
