25c30c05d3108adb1c2d2a4bb79eb28b7d4a2c1d
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-12 12:12:26 +0000

    refactor: reduces profile view to only what's needed since project restructure

--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,8 @@
-import stripe
-import json
-from django.conf import settings
-from django.shortcuts import (
-    render, reverse, get_object_or_404, redirect, HttpResponse)
-from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
 from .models import UserProfile
 from charities.models import Charity
 from subscriptions.models import Subscription
-from .forms import UserForm, UserProfileForm
 
 
 def get_charity_favs(user_profile):
@@ -24,14 +17,10 @@ def get_charity_favs(user_profile):
 
 @login_required
 def profile(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
     # Get user and UserProfile
     user = request.user
     user_profile = get_object_or_404(UserProfile, user=user)
-    user_form = UserForm(instance=user)
-    user_profile_form = UserProfileForm(instance=user_profile)
     # Check or create a subscription object
     subscription, created = Subscription.objects.get_or_create(user=user)
     # Get charity favs list
@@ -41,14 +30,9 @@ def profile(request):
     active_tab = request.GET.get('tab', 'myDetails')
 
     context = {
-        'user_form': user_form,
-        'user_profile_form': user_profile_form,
         'active_tab': active_tab,
         'charity_favs': charity_favs,
         'subscription': subscription,
-        'stripe_public_key': stripe_public_key,
-        'stripe_secret_key': stripe_secret_key,
-        'client_secret': 'test client secret',
     }
 
     return render(request, 'profiles/profile.html', context)
