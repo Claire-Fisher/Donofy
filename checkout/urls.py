cb3dad7066df694ab3e2cb69114dedbee32fee8d
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-13 17:58:26 +0000

    add: path to billing success

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('checkout/', views.checkout, name='checkout')
+    path('checkout/', views.checkout, name='checkout'),
+    path(
+        'billing-success/<donation_number>',
+        views.billing_success,
+        name='billing_success'
+    ),
 ]
