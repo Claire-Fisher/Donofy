72534e44f2a6782c1a744b5db7c1612d1854464e
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-16 12:19:08 +0000

    chore: update the donation obj admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -3,19 +3,36 @@ from .models import Donation
 
 
 class DonationAdmin(admin.ModelAdmin):
-    list_display = (
-        'user_profile',
-        'date',
-        'total',
-        'donation_breakdown'
-    )
-    search_fields = ('user', 'date')
+    list_display = ('full_name', 'donation_number', 'date', 'total')
+    
+    search_fields = ('full_name', 'donation_number', 'email')
 
     fieldsets = (
         (None, {
-            'fields': ('user_profile', 'date', 'total', 'donation_breakdown')
+            'fields': (
+                'donation_number',
+                'stripe_pid',
+                'user_profile',
+                'full_name',
+                'email',
+                'phone_number',
+                'country',
+                'postcode',
+                'town_or_city',
+                'street_address1',
+                'street_address2',
+                'county',
+                'total',
+                'donation_breakdown',
+                'date',
+            ),
         }),
     )
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['donation_number', 'date']
+        return []
 
 
 admin.site.register(Donation, DonationAdmin)
