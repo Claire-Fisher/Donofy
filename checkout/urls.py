1d45c6b8bde0e6d1ce191a5ff8798994f6cc6e0d
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-16 12:19:41 +0000

    chore: corrects the billing-success url

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('checkout/', views.checkout, name='checkout'),
     path(
-        'billing-success/<donation_number>',
+        'billing_success/<donation_number>',
         views.billing_success,
         name='billing_success'
     ),
