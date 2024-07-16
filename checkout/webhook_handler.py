12000ce03a7f34d13589056458617f2a8c7c57bc
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-16 16:47:39 +0000

    chore: minor edit to initialising intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -19,8 +19,8 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
-        intent = event['data']['object']
-        print('Payment Intent Succeeded:', intent)
+        intent = event.data.object
+        print(intent)
 
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
