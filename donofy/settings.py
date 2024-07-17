e65808be8ff21b974b20b9a4d77c81e9f015e8ea
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-17 15:23:29 +0000

    add: new secret_key

--- donofy/settings.py
+++ donofy/settings.py
@@ -24,8 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-0((_u-3)f%gxva&0f*8ufut&gj$$+9y-46m@$^1r=p3cyo1d4i')
+SECRET_KEY = config('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
