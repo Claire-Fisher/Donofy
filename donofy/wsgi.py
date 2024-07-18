ec452d23f1cce3d3db886fe0eb05b6a79bfc9335
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-18 12:51:40 +0000

    chore: uninstalled whitenoise

--- donofy/wsgi.py
+++ donofy/wsgi.py
@@ -1,9 +1,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'donofy.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
