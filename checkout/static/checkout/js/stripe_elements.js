8e46e54ca0c78557e36e225c200899f3434b1a65
Claire Fisher <cafisher83@yahoo.co.uk>
2024-07-19 16:26:35 +0000

    chore: fixing stripe functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,9 +1,5 @@
-var stripePublicKey = JSON.parse(document.getElementById('id_stripe_public_key').textContent);
-var clientSecret = document.getElementById('id_client_secret').value;
-
-console.log('Stripe public key:', stripePublicKey);
-console.log('Client secret:', clientSecret);
-
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 
@@ -26,7 +22,6 @@ var style = {
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
 
-
 // Handle realtime validation errors on the card element
 card.addEventListener('change', function (event) {
     var errorDiv = document.getElementById('card-errors');
@@ -35,8 +30,7 @@ card.addEventListener('change', function (event) {
             <span class="icon" role="alert">
                 <i class="fas fa-times"></i>
             </span>
-            <span>${event.error.message}</span>
-        `;
+            <span>${event.error.message}</span>`;
         $(errorDiv).html(html);
     } else {
         errorDiv.textContent = '';
@@ -48,64 +42,28 @@ var form = document.getElementById('payment-form');
 
 form.addEventListener('submit', function(ev) {
     ev.preventDefault();
-    card.update({ 'disabled': true});
+    card.update({ 'disabled': true });
     $('#submit-button').attr('disabled', true);
-    $('#payment-form').fadeToggle(100);
-    $('#loading-overlay').fadeToggle(100);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+            var html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
 
-    var saveInfo = Boolean($('#id-save-info').attr('checked'));
-    // From using {% csrf_token %} in the form
-    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
-    var postData = {
-        'csrfmiddlewaretoken': csrfToken,
-        'client_secret': clientSecret,
-        'save_info': saveInfo,
-    };
-    var url = '/checkout/cache_checkout_data/';
-
-    $.post(url, postData).done(function() {
-        stripe.confirmCardPayment(clientSecret, {
-            payment_method: {
-                card: card,
-                billing_details: {
-                    name: $.trim(form.full_name.value),
-                    email: $.trim(form.email.value),
-                    phone: $.trim(form.phone_number.value),
-                    address:{
-                        line1: $.trim(form.street_address1.value),
-                        line2: $.trim(form.street_address2.value),
-                        city: $.trim(form.town_or_city.value),
-                        country: $.trim(form.country.value),
-                        state: $.trim(form.county.value),
-                    }
-                }
+            card.update({ 'disabled': false });
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
             }
-        }).then(function(result) {
-            if (result.error) {
-                var errorDiv = document.getElementById('card-errors');
-                var html = `
-                    <span class="icon" role="alert">
-                    <i class="fas fa-times"></i>
-                    </span>
-                    <span>${result.error.message}</span>`;
-                $(errorDiv).html(html);
-                $('#payment-form').fadeToggle(100);
-                $('#loading-overlay').fadeToggle(100);
-                card.update({ 'disabled': false});
-                $('#submit-button').attr('disabled', false);
-            } else {
-                if (result.paymentIntent.status === 'succeeded') {
-                    var stripePidInput = document.createElement('input');
-                    stripePidInput.setAttribute('type', 'hidden');
-                    stripePidInput.setAttribute('name', 'stripe_pid');
-                    stripePidInput.setAttribute('value', result.paymentIntent.id);
-                    form.appendChild(stripePidInput);
-                    form.submit();
-                }
-            }
-        });
-    }).fail(function () {
-        // Just reload the page, the error will be in django messages
-        location.reload();
+        }
     });
 });
--- checkout/views.py
+++ checkout/views.py
@@ -1,46 +1,30 @@
 from django.shortcuts import (
     render, redirect, reverse, get_object_or_404, HttpResponse
 )
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
 from django.contrib import messages
 from django.conf import settings
 from .forms import DonationForm
 from .models import Donation
 from profiles.models import UserProfile, Subscription
 import stripe
-from django.db import IntegrityError
 
 
 @require_POST
+@csrf_exempt
 def cache_checkout_data(request):
     try:
         pid = request.POST.get('client_secret').split('_secret')[0]
         stripe.api_key = settings.STRIPE_SECRET_KEY
-        donation_data = {
-            'full_name': request.POST.get('full_name', ''),
-            'email': request.POST.get('email', ''),
-            'phone_number': request.POST.get('phone_number', ''),
-            'country': request.POST.get('country', ''),
-            'postcode': request.POST.get('postcode', ''),
-            'town_or_city': request.POST.get('town_or_city', ''),
-            'street_address1': request.POST.get('street_address1', ''),
-            'street_address2': request.POST.get('street_address2', ''),
-            'county': request.POST.get('county', ''),
-            'total': request.POST.get('total', ''),
-        }
-        request.session['donation'] = donation_data
+        user = request.user
+        subscription = get_object_or_404(Subscription, user=user)
+        amount = round(subscription.sub_total * 100)
+
+        # Create a Payment Intent
         stripe.PaymentIntent.modify(pid, metadata={
-            'full_name': donation_data['full_name'],
-            'email': donation_data['email'],
-            'phone_number': donation_data['phone_number'],
-            'country': donation_data['country'],
-            'postcode': donation_data['postcode'],
-            'town_or_city': donation_data['town_or_city'],
-            'street_address1': donation_data['street_address1'],
-            'street_address2': donation_data['street_address2'],
-            'county': donation_data['county'],
-            'total': donation_data['total'],
-            'save_info': request.POST.get('save_info'),
+            'amount': amount,
             'username': request.user.username,
         })
         return HttpResponse(status=200)
@@ -50,13 +34,15 @@ def cache_checkout_data(request):
         return HttpResponse(content=e, status=400)
 
 
+@login_required
 def checkout(request):
-
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     user = request.user
     user_profile = get_object_or_404(UserProfile, user=user)
     subscription = get_object_or_404(Subscription, user=user)
+    total = subscription.sub_total
 
     if subscription.sub_total == 0:
         messages.error(
@@ -64,104 +50,88 @@ def checkout(request):
             'You need to set your donation preferences '
             'before you can make a donation.'
         )
-
         return redirect('home')
 
+    if request.method == 'POST':
+        form_data = {
+            'full_name': request.POST.get('full_name', ''),
+            'email': request.POST.get('email', ''),
+            'phone_number': request.POST.get('phone_number', ''),
+            'country': request.POST.get('country', ''),
+            'postcode': request.POST.get('postcode', ''),
+            'town_or_city': request.POST.get('town_or_city', ''),
+            'street_address1': request.POST.get('street_address1', ''),
+            'street_address2': request.POST.get('street_address2', ''),
+            'county': request.POST.get('county', ''),
+        }
+
+        donation_form = DonationForm(form_data)
+
+        if donation_form.is_valid():
+            donation = donation_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            print(pid)
+            donation.stripe_pid = pid
+            donation.user_profile = user_profile
+            donation.total = total
+            donation.donation_breakdown = subscription.sub_breakdown
+            donation.save()
+
+            return redirect(
+                reverse('billing_success', args=[donation.donation_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
     else:
-        donation_form = DonationForm()
-        client_secret = ''
-
-        if request.method == 'POST':
-            form_data = {
-                'full_name': request.POST.get('full_name', ''),
-                'email': request.POST.get('email', ''),
-                'phone_number': request.POST.get('phone_number', ''),
-                'country': request.POST.get('country', ''),
-                'postcode': request.POST.get('postcode', ''),
-                'town_or_city': request.POST.get('town_or_city', ''),
-                'street_address1': request.POST.get('street_address1', ''),
-                'street_address2': request.POST.get('street_address2', ''),
-                'county': request.POST.get('county', ''),
-            }
-            donation_form = DonationForm(form_data)
-
-            if donation_form.is_valid():
-                donation = donation_form.save(commit=False)
-                donation.total = subscription.sub_total
-                donation.donation_breakdown = subscription.sub_breakdown
-                donation.user_profile = user_profile
-                donation.stripe_pid = request.POST.get('stripe_pid')
-
-                try:
-                    donation.save()
-                    request.session['save_info'] = 'save-info' in request.POST
-                    return redirect(
-                        reverse('billing_success', args=[donation.donation_number])
-                    )
-                except IntegrityError:
-                    messages.error(
-                        request,
-                        'A donation with this Stripe ID already exists.'
-                        'Please try again.'
-                    )
-                    return redirect(reverse('checkout'))
-
-            else:
-                messages.error(
-                    request,
-                    'There was an error with your form.'
-                    'Please double check your information.'
-                )
-
-        if request.user.is_authenticated:
-            donation_form = DonationForm(initial={
-                'full_name': user_profile.user.get_full_name(),
-                'email': user_profile.user.email,
-                'phone_number': user_profile.phone_num,
-                'postcode': user_profile.post_code_zip,
-                'town_or_city': user_profile.town_or_city,
-                'street_address1': user_profile.street_address_1,
-                'street_address2': user_profile.street_address_2,
-                'county': user_profile.county,
-                'country': user_profile.country,
-            })
-
-        stripe_total = round(subscription.sub_total * 100)
+        total_pence = round(total * 100)
         stripe.api_key = stripe_secret_key
         intent = stripe.PaymentIntent.create(
-            amount=stripe_total,
-            currency=settings.STRIPE_CURRENCY,
-        )
-        client_secret = intent.client_secret
-
-        if not stripe_public_key:
-            messages.warning(
-                request,
-                'Stripe public key is missing.'
-                'Did you forget to set it in your environment?'
+                amount=total_pence,
+                currency=settings.STRIPE_CURRENCY,
             )
+        print(f'PAYMENT INTENT = {intent}')
 
-        template = 'checkout/checkout.html'
-        context = {
-            'donation_form': donation_form,
-            'stripe_public_key': stripe_public_key,
-            'client_secret': client_secret,
-            'subscription': subscription,
-            'subscription_breakdown': subscription.sub_breakdown,
-            'total': subscription.sub_total,
-        }
+    # Prepopulate checkout form if user has their info saved
+    if request.user.is_authenticated:
+        donation_form = DonationForm(initial={
+            'full_name': user_profile.user.get_full_name(),
+            'email': user_profile.user.email,
+            'phone_number': user_profile.phone_num,
+            'postcode': user_profile.post_code_zip,
+            'town_or_city': user_profile.town_or_city,
+            'street_address1': user_profile.street_address_1,
+            'street_address2': user_profile.street_address_2,
+            'county': user_profile.county,
+            'country': user_profile.country,
+        })
+    else:
+        donation_form = DonationForm()
+
+    if not stripe_public_key:
+        messages.warning(
+            request,
+            'Stripe public key is missing. '
+            'Did you forget to set it in your environment?'
+        )
+
+    template = 'checkout/checkout.html'
+    context = {
+        'subscription': subscription,
+        'donation_form': donation_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
 
     return render(request, template, context)
 
 
+@login_required
 def billing_success(request, donation_number):
     """
     Handle successful donations
     """
     donation = get_object_or_404(Donation, donation_number=donation_number)
-
-    if request.user.is_authenticated:
-        profile = UserProfile.objects.get(user=request.user)
+    profile = UserProfile.objects.get(user=request.user)
 
     messages.success(
         request,
@@ -169,9 +139,6 @@ def billing_success(request, donation_number):
         f'Your donation number is {donation_number}. '
         f'A confirmation email will be sent to {donation.email}.'
     )
-
-    if 'save_info' in request.session:
-        del request.session['save_info']
 
     template = 'checkout/billing_success.html'
     context = {
